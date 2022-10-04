c49c6f491ac35e63d9fa22c453f3e879fbbc8a23
Edmond Luu <eluu472@gmail.com>
2022-10-03 17:03:04 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const [joke, setJoke] = React.useState();
+
+  function handleClick() {
+    fetch('https://icanhazdadjoke.com', {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => setJoke(data.joke))
+  }
+
+  function handleReset() {
+    setJoke();
+  }
+
+  function handleCopy() {
+    navigator.clipboard.writeText(joke)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Dad Joke Generator</h1>
+      <div className="jokeField">
+        <p className="jokeText" onClick={handleCopy}>{joke}</p>
+      </div>
+      <p className="caption">Click on the joke to copy it to your clipboard</p>
+      <div className="button generateButton" onClick={handleClick}>Generate Joke</div>
+      <div className="button resetButton" onClick={handleReset}>Reset</div>
+      <div className="footer">
+        <p>Made completely from scratch with ❤️ by <a href="https://edmond-luu.github.io" target="_blank" rel="noreferrer">Edmond Luu</a></p>
+      </div>
     </div>
   );
 }
